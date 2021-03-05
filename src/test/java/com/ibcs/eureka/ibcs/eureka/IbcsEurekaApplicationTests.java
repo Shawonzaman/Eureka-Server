1f08ebf0b868086d91519ea81442f523a9bdc89f
shawonzaman <bdshawon63@gmail.com>
2021-03-05 22:37:00 +0600

    Initial commit.

---
+++ src/test/java/com/ibcs/eureka/ibcs/eureka/IbcsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibcs.eureka.ibcs.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbcsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
